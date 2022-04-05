881ed602c03da761cf83c3d58bd4a77360578f4c
Krishna Kanhaiya <25430514+krishnaarga@users.noreply.github.com>
2022-04-05 19:01:55 +0530

    Create call page for function
    Create call page for function

---
+++ call-function.php
@@ -0,0 +1,18 @@
+$('.students').on('click', function(){
+  let id = $(this).attr('id');
+  let first_name = $('#first_name'+id).val();
+  let last_name = $('#last_name'+id).val();
+              
+  let m_card_data = { id, first_name, last_name }
+  $('#alert').html('<div class="alert alert-warning font-weight-bold" role="alert">Processing...</div>').slideDown("slow");
+  $.ajax({
+    url:ajaxurl,
+    type:'POST',
+    data:{action:'update_mcard_using_id', m_card_data},
+    success:function(data){
+      // console.log(data)
+      $('#alert').html('<div class="alert alert-success font-weight-bold" role="alert">M-Card Updated Successfully...</div>').slideDown("slow");
+      setTimeout(() => { $('#alert').slideUp("slow") }, 5000);
+    }
+  });
+});
